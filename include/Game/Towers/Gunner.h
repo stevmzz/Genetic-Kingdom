82ec7e5bca46677d9a0030182afd2786f4594f29
Josumveg <josepabloumana@gmail.com>
2025-05-23 19:39:51 +0000

    [FEAT] Sistema de economia

--- include/Game/Towers/Gunner.h
+++ include/Game/Towers/Gunner.h
@@ -11,20 +11,8 @@
 
 class Gunner : public Tower {
 public:
-    Gunner() : Tower(75, 200.0f, 0.5f, 7.0f) {
-        texture.loadFromFile("assets/images/towers/Gunner.png");
-        sprite.setTexture(texture);
-
-        // Centrar el origen del sprite
-        sf::FloatRect bounds = sprite.getLocalBounds();
-        sprite.setOrigin(bounds.width / 2.f, bounds.height / 2.f);
-
-        // Escalar el sprite (ajusta el tamaño a tu celda)
-        float scaleFactor = 40.f / bounds.width; // Asumiendo que la celda mide 40x40 px
-        sprite.setScale(scaleFactor, scaleFactor);
-    }
-
-    void attack(Enemy& enemy) override;
+    Gunner();
+    void attack(Enemy& enemy, const std::vector<std::unique_ptr<Enemy>>&) override;
     std::string type() const override { return "Gunner"; }
 };
 
--- src/Game/Towers/Archer.cpp
+++ src/Game/Towers/Archer.cpp
@@ -5,16 +5,61 @@
 #include "Game/Towers/Archer.h"
 #include "Game/Enemies/Enemy.h"
 
-void Archer::attack(Enemy& enemy) {
+
+Archer::Archer() : Tower(
+    50, // cost
+    30, // damage
+    300.0f, // range
+    1.5f, // attack speed
+    5.0f // special cooldown
+    ) {
+    texture.loadFromFile("assets/images/towers/Archer.png");
+    sprite.setTexture(texture);
+
+    // Centrar el origen del sprite
+    sf::FloatRect bounds = sprite.getLocalBounds();
+    sprite.setOrigin(bounds.width / 2.f, bounds.height / 2.f);
+
+    // Escalar el sprite (ajusta el tamaño a tu celda)
+    float scaleFactor = 40.f / bounds.width; // Asumiendo que la celda mide 40x40 px
+    sprite.setScale(scaleFactor, scaleFactor);
+
+}
+
+void Archer::attack(Enemy& enemy, const std::vector<std::unique_ptr<Enemy>>&) {
+    // ataque normal
     float elapsed = attackClock.getElapsedTime().asSeconds();
     if (elapsed >= 1.0f / attackSpeed) {
-        std::cout << "Archer shoots causing " << damage << " damage.\n";
         enemy.receiveDamage(damage);
         attackClock.restart(); // reinicia el temporizador
     }
-    // if (specialClock.getElapsedTime().asSeconds() >= specialCooldown) {
-    //     // lanzar ataque especial (si aplica)
-    //     std::cout << type() << " uses special ability!\n";
-    //     specialClock.restart();
-    // }
+
+    // ataque especial con probabilidad
+    // el ataque especial es una rafaga rapida de 3 tiros a un enemigo
+    float specialElapsed = specialClock.getElapsedTime().asSeconds();
+    if (!burstActive && specialElapsed >= specialCooldown) {
+        float roll = static_cast<float>(rand()) / RAND_MAX;
+        if (roll <= specialChance) {
+            std::cout << "Archer fires a burst attack\n";
+            burstActive = true;
+            burstShotsFired = 0;
+            burstClock.restart();
+        }
+        specialClock.restart();
+    }
+
+    // ejecutar ráfaga si está activa
+    if (burstActive && burstShotsFired < totalBurstShots) {
+        if (burstClock.getElapsedTime().asSeconds() >= burstInterval) {
+            std::cout << "Burst shot " << (burstShotsFired + 1) << " hits for " << damage*0.8 << " damage.\n";
+            enemy.receiveDamage(damage*0.8);
+            burstShotsFired++;
+            burstClock.restart();
+        }
+    }
+
+    // terminar la ráfaga
+    if (burstActive && burstShotsFired >= totalBurstShots) {
+        burstActive = false;
+    }
 }
